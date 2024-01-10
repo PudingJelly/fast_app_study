a1c9ac6b80331a487ec6a41f2e3f078a2d3c0064
Changju Mun <changju7857@gmail.com>
2024-01-10 16:58:00 +0900

    24.01.10 홈스크린 구성

--- ttoss_app/lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ ttoss_app/lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -11,35 +11,35 @@ final bankAccountToss = BankAccount(bankToss, 500000);
 final bankAccountKakao =
     BankAccount(bankKakao, 1000000, accountTypeName: "입출금통장");
 
-main() {
-  // print(bankAccounts[0].accountTypeName);
-  //
-  // for (final item in bankAccounts) {
-  //   print(item.accountTypeName);
-  // }
-  final shinhanBank = bankMap["shinhan1"];
-  // print(shinhanBank == bankAccountShinhan1);
-
-  for(final entry in bankMap.entries) {
-    print("${entry.key}:${entry.value.accountTypeName ?? entry.value.bank.name}");
-  }
-
-  print(bankSet.contains(bankAccountShinhan1));
-
-  print(bankAccounts.contains(bankAccountShinhan1));
-
-  // List contains와 Set contains의 차이
-  // List는 모든 리스트를 한바퀴 돌고 나서 값의 존재유무 파악,
-  // Set은 단순히 값의 존재유무 바로 파악 가능
-  // 결국 로직 속도의 차이
-
-  // List와 Map,Set의 차이 => List는 중복 허용, Map,Set은 중복 허용X
-
-  // List를 Set으로 변환
-  bankAccounts.toSet();
-  // Set을 List로 변환
-  bankSet.toList();
-}
+// main() {
+//   // print(bankAccounts[0].accountTypeName);
+//   //
+//   // for (final item in bankAccounts) {
+//   //   print(item.accountTypeName);
+//   // }
+//   final shinhanBank = bankMap["shinhan1"];
+//   // print(shinhanBank == bankAccountShinhan1);
+//
+//   for(final entry in bankMap.entries) {
+//     print("${entry.key}:${entry.value.accountTypeName ?? entry.value.bank.name}");
+//   }
+//
+//   print(bankSet.contains(bankAccountShinhan1));
+//
+//   print(bankAccounts.contains(bankAccountShinhan1));
+//
+//   // List contains와 Set contains의 차이
+//   // List는 모든 리스트를 한바퀴 돌고 나서 값의 존재유무 파악,
+//   // Set은 단순히 값의 존재유무 바로 파악 가능
+//   // 결국 로직 속도의 차이
+//
+//   // List와 Map,Set의 차이 => List는 중복 허용, Map,Set은 중복 허용X
+//
+//   // List를 Set으로 변환
+//   bankAccounts.toSet();
+//   // Set을 List로 변환
+//   bankSet.toList();
+// }
 // List
 final bankAccounts = [
   bankAccountShinhan1,
