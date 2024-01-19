f250fe1556d0f677675925094860ebe7c7a43515
Changju Mun <changju7857@gmail.com>
2024-01-19 15:37:53 +0900

    24.01.19 todo_app convert bloc by Cubit

--- fast_app_base-main/lib/screen/main/tab/todo/widget/w_todo_list.dart
+++ fast_app_base-main/lib/screen/main/tab/todo/widget/w_todo_list.dart
@@ -1,21 +1,22 @@
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/data/memory/bloc/todo_bloc_state.dart';
 import 'package:fast_app_base/screen/main/tab/todo/widget/w_todo_item.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TodoList extends StatelessWidget with TodoDataProvider {
-  TodoList({super.key});
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => todoData.todoList.isEmpty
+    return BlocBuilder<TodoCubit, TodoBlocState>(builder: (context, state) {
+      return state.todoList.isEmpty
           ? '할 일을 작성 해보세요'.text.size(30).makeCentered()
           : SingleChildScrollView(
-              child: Column(
-                children: todoData.todoList.map((e) => TodoItem(e)).toList(),
-              ),
-            ),
-    );
+        child: Column(
+          children: state.todoList.map((e) => TodoItem(e)).toList(),
+        ),
+      );
+    });
   }
 }
