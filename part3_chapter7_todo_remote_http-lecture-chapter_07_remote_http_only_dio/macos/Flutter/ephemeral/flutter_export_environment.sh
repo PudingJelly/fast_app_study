12ebb84a1d317d4852eeb98de7da245d0cd673ab
Changju Mun <changju7857@gmail.com>
2024-01-22 18:19:02 +0900

    24.01.22

---
+++ part3_chapter7_todo_remote_http-lecture-chapter_07_remote_http_only_dio/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\remis\fvm\versions\3.10.0"
+export "FLUTTER_APPLICATION_PATH=C:\work\study\fast_campus_flutter\part3_chapter7_todo_remote_http-lecture-chapter_07_remote_http_only_dio"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
